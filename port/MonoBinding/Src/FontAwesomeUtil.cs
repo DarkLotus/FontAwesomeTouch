593c2bd4c634395c161eb7859d5d421bb279a956
Junichi OKADOME <tome@tomesoft.net>
2013-01-16 06:36:25 +0900

    Changed getting scale factor from MainScreen on Mac Apply the apache 2.0 license description

--- port/MonoBinding/Src/FontAwesomeUtil.cs
+++ port/MonoBinding/Src/FontAwesomeUtil.cs
@@ -1,11 +1,29 @@
+// 
+// FontAwesomeUtil.cs
+//  
+// Author:
+//       Junichi OKADOME (tome@tomesoft.net)
+// 
+// Copyright 2012-2013 tomesoft.net
+//	
+//	Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//		
+//		http://www.apache.org/licenses/LICENSE-2.0
+//		
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #define USE_WEAKREFERENCE
 using System;
 using System.IO;
 using System.Linq;
-//using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
-//using System.Threading.Tasks;
 using System.Drawing;
 
 #if MONOTOUCH
@@ -139,7 +157,7 @@ namespace FontAwesome
 		                                , float? scaleOrNull = null
 		                                , PointF? offsetOrNull = null)
 		{
-			float scale = scaleOrNull ?? ScaleForDevice;
+			float scale = scaleOrNull ?? ScaleOfMainScreen;
 			float fontSize = fontSizeUnit * scale;
 			var ctfont = new CTFont (FontAwesomeUtil.Font, fontSize, CGAffineTransform.MakeIdentity ());
 			float fontMetric = ctfont.AscentMetric + ctfont.DescentMetric;
@@ -200,18 +218,20 @@ namespace FontAwesome
 			}
 		}
 
-		static float? _scaleForDevice;
-		static float ScaleForDevice {
+		static float? _scaleOfMainScreen;
+		static float ScaleOfMainScreen {
 			get {
-				if (_scaleForDevice == null)
+				if (_scaleOfMainScreen == null)
 				{
 					NSThread.MainThread.InvokeOnMainThread (() => {
 #if MONOTOUCH
 						_scaleForDevice = UIScreen.MainScreen.Scale;
+#elif MONOMAC
+						_scaleOfMainScreen = NSScreen.MainScreen.BackingScaleFactor;
 #endif
 					});
 				}
-				return _scaleForDevice ?? 1f;
+				return _scaleOfMainScreen ?? 1f;
 			}
 		}
 
@@ -221,7 +241,7 @@ namespace FontAwesome
 		                                          , float? scaleOrNull = null
 		                                          , PointF? offsetOrNull = null)
 		{
-			float scale = scaleOrNull ?? ScaleForDevice;
+			float scale = scaleOrNull ?? ScaleOfMainScreen;
 			float fontSize = sizeUnit * scale * 0.9f; // 0.9f is a magic value for adjust
 			var ctfont = new CTFont (FontAwesomeUtil.Font, fontSize, CGAffineTransform.MakeIdentity ());
 			float fontMetric = ctfont.AscentMetric + ctfont.DescentMetric;
